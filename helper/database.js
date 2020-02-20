4b80b3245737a06d1336e14dd551967f87deacb0
Shappy <jtshappy@labs.neit.edu>
2020-02-20 12:07:57 -0500

    corrected mongoURL

--- helper/database.js
+++ helper/database.js
@@ -1,11 +1,11 @@
 if(process.env.NODE_ENV === "production"){
     module.exports ={
         //connection to cloud mongodb server 
-        mongoURI:"mongodb://jordan:jordan@cluster0-ezrcu.mongodb.net/test?retryWrites=true&w=majority"
+        mongoURI:"mongodb+srv://HapShapIncorporated:nitsujSHAPPY2298%40%40%28%2A@web-api-for-games-f34dp.mongodb.net/test?retryWrites=true&w=majority"
     }
 }
 else{
     module.exports ={
-        mongoURI:"mongodb://localhost:27017/gamelibrary"
+        mongoURI:"mongodb://localhost:27017/GameLibrary"
     }
 }
